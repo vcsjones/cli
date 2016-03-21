858e6f5dd7f8b7699bd293aed5ff0ecc538b9a9b
David Fowler <davidfowl@gmail.com>
2016-03-21 09:03:40 -0700

    WIP

--- src/Microsoft.DotNet.ProjectModel/Properties/AssemblyInfo.cs
+++ src/Microsoft.DotNet.ProjectModel/Properties/AssemblyInfo.cs
@@ -8,18 +8,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.DotNet.ProjectModel")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.DotNet.ProjectModel")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("303677d5-7312-4c3f-baee-beb1a9bd9fe6")]
